dc7988c9269255941360a79aeb7a7f178233e0cf
chenjialuo <cjl@zhongmakj.com>
2017-12-11 16:25:53 +0800

    indent

--- amg8853.c
+++ amg8853.c
@@ -3,69 +3,71 @@
  *
  * Created: 2017/10/1 15:30:44
  *  Author: konar
- */ 
- 
- #include "amg8853.h"
+ */
 
- #include "twi.h"
+#include "amg8853.h"
 
- uint8_t *target_buff;
+#include "twi.h"
 
- // init
- void amg8853_init() {
-	twi_init();
- }
+uint8_t *target_buff;
 
- // read from twi buffer
- void amg8853_read_callback(uint8_t address, uint8_t *data) {
-	 uint8_t i;
-	 for (i=0; i<128; i++) {
-		 target_buff[i] = data[i];
-	 }
- }
+// init
+void amg8853_init() { twi_init(); }
 
- // receive 128 bytes data from amg8853 using twi
- void amg8853_twi_read_64_tem(uint8_t buff[]) {
-	target_buff = buff;
-	uint8_t data = AMG8853_TEM_START_ADDR;
-	twi_write(AMG8853_SENSOR_ADDR, &data, 1, NULL);
-	twi_wait();
-	twi_read(AMG8853_SENSOR_ADDR, 128, amg8853_read_callback);
-	twi_wait();
- }
+// read from twi buffer
+void amg8853_read_callback(uint8_t address, uint8_t *data) {
+    uint8_t i;
+    for (i = 0; i < 128; i++) {
+        target_buff[i] = data[i];
+    }
+}
 
-/* 
+// receive 128 bytes data from amg8853 using twi
+void amg8853_twi_read_64_tem(uint8_t buff[]) {
+    target_buff = buff;
+    uint8_t data = AMG8853_TEM_START_ADDR;
+    twi_write(AMG8853_SENSOR_ADDR, &data, 1, NULL);
+    twi_wait();
+    twi_read(AMG8853_SENSOR_ADDR, 128, amg8853_read_callback);
+    twi_wait();
+}
+
+/*
  * temperature convert
  * buff				in	data from amg8853 128 bytes
  * snumber			in	sensor index 0~63
- * sign				out	sign of the result 1:negative, 0:positive
+ * sign				out	sign of the result 
+ *                       1:negative, 0:positive 
  * integer_part		out integer part of the result 0~255
  * decimal_part		out decimal part of the result (multiply by 100) 0~99
  */
- void amg8853_tem_convert(uint8_t buff[], uint8_t snumber, uint8_t *sign, uint8_t *integer_part, uint8_t *decimal_part) {
+void amg8853_tem_convert(uint8_t buff[], uint8_t snumber, 
+    uint8_t *sign, uint8_t *integer_part, uint8_t *decimal_part) {
 
-	// read data
-	uint8_t lowByte = buff[snumber << 1];
-	uint8_t highByte = buff[(snumber << 1) + 1];
+    // read data
+    uint8_t lowByte = buff[snumber << 1];
+    uint8_t highByte = buff[(snumber << 1) + 1];
 
-	int16_t rst;
+    int16_t rst;
 
-	// charge sign
-	if (highByte & 0x08) {
-		// negative
-		*sign = 1;
-		// negative, two's complement, set bit for high-order bits
-		rst = (((((uint16_t)(highByte | 0xf0)) << 8) & 0xff00) | (((uint16_t)(lowByte)) & 0x00ff));
-		// to positive number
-		rst = -rst;
-	} else {
-		// positive
-		*sign = 0;
-		rst = (((((uint16_t)(highByte)) << 8) & 0xff00) | (((uint16_t)(lowByte)) & 0x00ff));
-	}
+    // charge sign
+    if (highByte & 0x08) {
+        // negative
+        *sign = 1;
+        // negative, two's complement, set bit for high-order bits
+        rst = (((((uint16_t)(highByte | 0xf0)) << 8) & 0xff00) |
+               (((uint16_t)(lowByte)) & 0x00ff));
+        // to positive number
+        rst = -rst;
+    } else {
+        // positive
+        *sign = 0;
+        rst = (((((uint16_t)(highByte)) << 8) & 0xff00) |
+               (((uint16_t)(lowByte)) & 0x00ff));
+    }
 
-	// decimal part
-	switch (rst & 0x03) {
+    // decimal part
+    switch (rst & 0x03) {
 		case 0x00:
 			*decimal_part = 0;
 			break;
@@ -84,9 +86,8 @@
 
 		default:
 			break;
-	}
+    }
 
-	// integer part
-	*integer_part = (uint8_t)((rst >> 2) & 0x00ff);
-
- }
+    // integer part
+    *integer_part = (uint8_t)((rst >> 2) & 0x00ff);
+}
--- amg8853.h
+++ amg8853.h
@@ -3,20 +3,18 @@
  *
  * Created: 2017/10/1 15:28:59
  *  Author: konar
- */ 
-
+ */
 
 #ifndef AMG8853_H_
 #define AMG8853_H_
 
 #include <stdint.h>
 
-#define AMG8853_DELAY			_delay_us(2)
-#define AMG8853_TEM_START_ADDR	0x80
-#define AMG8853_SENSOR_ADDR		0b1101000
-#define AMG8853_WRITE			(AMG8853_SENSOR_ADDR<<1)|0
-#define AMG8853_READ			(AMG8853_SENSOR_ADDR<<1)|1
-
+#define AMG8853_DELAY           _delay_us(2)
+#define AMG8853_TEM_START_ADDR  0x80
+#define AMG8853_SENSOR_ADDR     0b1101000
+#define AMG8853_WRITE           (AMG8853_SENSOR_ADDR << 1) | 0
+#define AMG8853_READ            (AMG8853_SENSOR_ADDR << 1) | 1
 
 // init
 void amg8853_init();
@@ -24,15 +22,16 @@ void amg8853_init();
 // receive 128 bytes data from amg8853 using twi
 void amg8853_twi_read_64_tem(uint8_t buff[]);
 
-/* 
+/*
  * temperature convert
  * buff				in	data from amg8853 128 bytes
  * snumber			in	sensor index 0~63
- * sign				out	sign of the result 1:negative, 0:positive
+ * sign				out	sign of the result 
+ *                       1:negative, 0:positive 
  * integer_part		out integer part of the result 0~255
  * decimal_part		out decimal part of the result (multiply by 100) 0~99
  */
-void amg8853_tem_convert(uint8_t buff[], uint8_t snumber, uint8_t *sign, uint8_t *integer_part, uint8_t *decimal_part);
-
+void amg8853_tem_convert(uint8_t buff[], uint8_t snumber, 
+    uint8_t *sign, uint8_t *integer_part, uint8_t *decimal_part);
 
 #endif /* AMG8853_H_ */
